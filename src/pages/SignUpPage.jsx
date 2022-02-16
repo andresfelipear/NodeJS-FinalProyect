2fa2b28af7625cb4533d86a8201053eb4d26b0e2
Andres Arevalo <andresfelipear@gmail.com>
2022-02-15 23:25:29 -0800

    installed bulma, created signup page

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,42 +1,117 @@
+import { useEffect, useState } from "react";
 import React from 'react'
+import {
+    Heading,
+    Button,
+    Notification,
+} from "react-bulma-components";
 
 export default function SignUpPage() {
-  return (
-    <main class="container mt-6">
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState(undefined);
+    const [confirmPassword, setConfirmPassword] = useState(undefined);
+    const [status, setStatus] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const [disabled, setDisabled] = useState(true);
 
-        <form action="/signup" method="post">
+    const onChange = (event) => {
+        if (event.target.name === "password") setPassword(event.target.value);
+        if (event.target.name === "confirmPassword")
+            setConfirmPassword(event.target.value);
+    };
 
-            <div class="field">
-                <label class="label" for="username">Username</label>
-                <div class="control">
-                    <input class="input" type="text" name="username" />
+    const submit = () => {
+        const body = { username, email, password };
+        setIsLoading(true);
+        fetch("http://localhost:8000/api/user/signup", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(body),
+        })
+            .then((res) => {
+                if (!res.ok) {
+                    setIsLoading(false);
+                    throw new Error(res.status);
+                }
+                return res.json();
+            })
+            .then((data) => {
+                setStatus("success");
+                setIsLoading(false);
+            })
+            .catch((err) => {
+                setStatus("error");
+                setIsLoading(false);
+            });
+    };
+
+    useEffect(() => {
+        if (password && confirmPassword) {
+            setDisabled(password !== confirmPassword)
+        }
+
+        return () => {
+            setStatus("")
+            setIsLoading(false)
+        }
+    }, [password, confirmPassword])
+
+    return (
+        <main class="container mt-6">
+
+
+                {status === "error" && (
+                    <Notification>
+                        <Heading>Error Signing Up!</Heading>
+                        Username and/or Email already exists
+                        <Button remove role="alertdialog" onClick={() => setStatus("")} />
+                    </Notification>
+                )}
+                {status === "success" && (
+                    <Notification>
+                        <Heading>Signed Up Successfully</Heading>
+                        Click <a>here</a> to go to Login page
+                        <Button remove role="alertdialog" onClick={() => setStatus("")} />
+                    </Notification>
+                )}
+
+                <div class="field">
+                    <label class="label" for="username">Username</label>
+                    <div class="control">
+                        <input class="input" type="text" name="username" onChange={(e) => setUsername(e.target.value)} />
+                    </div>
                 </div>
-            </div>
-            <div class="field">
-                <label class="label" for="username">Email</label>
-                <div class="control">
-                    <input class="input" type="email" name="email" />
+                <div class="field">
+                    <label class="label" for="email">Email</label>
+                    <div class="control">
+                        <input class="input" type="email" name="email" onChange={(e) => setEmail(e.target.value)} />
+                    </div>
                 </div>
-            </div>
-            <div class="field">
-                <label class="label" for="username">Password</label>
-                <div class="control">
-                    <input class="input" type="password" name="password" />
+                <div class="field">
+                    <label class="label" for="password">Password</label>
+                    <div class="control">
+                        <input class="input" type="password" name="password" onChange={onChange} />
+                    </div>
                 </div>
-            </div>
-            <div class="field">
-                <label class="label" for="username">Confirm Password</label>
-                <div class="control">
-                    <input class="input" type="password" name="confirmPassword" />
+                <div class="field">
+                    <label class="label" for="confirmPassword">Confirm Password</label>
+                    <div class="control">
+                        <input class="input" type="password" name="confirmPassword" onChange={onChange} />
+                    </div>
                 </div>
-            </div>
 
-            <button class="button is-primary" type="submit">
-                Sign Up
-            </button>
+                <button 
+                    class={`button ${isLoading && 'is-loading'} ${!isLoading && 'is-primary'}`}
+                    disabled={disabled}
+                    onClick={submit}
+                    type="submit">
+                    Sign Up
+                </button>
 
-        </form>
+                {isLoading && <span>Loading...</span>}
 
-    </main>
-  )
+
+        </main>
+    )
 }
