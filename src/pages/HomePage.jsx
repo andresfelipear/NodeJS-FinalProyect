8a384508bfa3c136ca2bd33ff11e353a4e81be39
Andres Arevalo <andresfelipear@gmail.com>
2022-02-18 09:44:49 -0800

    moved files to another folder. Show posts done!

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,15 +6,6 @@ import {
   Notification,
 } from "react-bulma-components";
 
-import img1 from '../assets/images/img1.jpg'
-import img2 from '../assets/images/img2.jpg'
-import img3 from '../assets/images/img3.jpg'
-import img4 from '../assets/images/img4.jpg'
-import img5 from '../assets/images/img5.jpg'
-import img6 from '../assets/images/img6.jpg'
-import img7 from '../assets/images/img7.jpg'
-import img8 from '../assets/images/img8.jpg'
-
 function HomePage() {
 
   const [error, setError] = useState("");
@@ -41,7 +32,7 @@ function HomePage() {
   }, [posts])
 
   useEffect(() => {
-    if (posts.length == 0) {
+    if (posts.length === 0) {
       fetchPosts();
     }
   }, [fetchPosts, posts]);
@@ -72,7 +63,7 @@ function HomePage() {
                 <div className="card">
                   <div className="card-image">
                     <figure className="image is-4by3">
-                      <img src={post.imageUrl}
+                      <img src={`/images/${post.imageUrl}.jpg`}
                         alt={post.title} width="100px" />
                     </figure>
                   </div>
