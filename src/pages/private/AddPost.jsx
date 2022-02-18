b0e0375ef6503f090f1d05077caf84e730ba6dd3
Andres Arevalo <andresfelipear@gmail.com>
2022-02-17 18:39:35 -0800

    defined data for send request to the server

--- src/pages/private/AddPost.jsx
+++ src/pages/private/AddPost.jsx
@@ -1,14 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 
 function AddPost() {
+
+
     const post = {
         title: "title",
         description: "description",
         imageUrl: "imageUrl"
     }
-
+    const [title, setTitle] = useState("")
+    const [imageUrl, setImageUrl] = useState("")
+    const [description, setDescription] = useState("")
     const [editing, setEditing] = useState(false)
+    const [disabled, setDisabled] = useState(true);
+
+    useEffect(() => {
+        if ( title && imageUrl && description) {
+            setDisabled(false)
+        }
+    }, [title, imageUrl, description])
 
 
     return (
@@ -17,20 +28,20 @@ function AddPost() {
                 <div class="field">
                     <label className="label" for="title">Title</label>
                     <div className="control">
-                        <input className="input" type="text" name="title" value={editing ? post.title : ""} />
+                        <input className="input" type="text" name="title" value={editing ? post.title : title} onChange={(e) => setTitle(e.target.value)} />
                     </div>
                 </div>
                 <div className="field">
                     <label className="label" for="title">Image (url)</label>
                     <div className="control">
-                        <input className="input" type="text" name="imageUrl" value={editing ? post.imageUrl : ""} />
+                        <input className="input" type="text" name="imageUrl" value={editing ? post.imageUrl : imageUrl} onChange={(e) => setImageUrl(e.target.value)} />
                     </div>
                 </div>
                 <div className="field">
                     <label className="label" for="title">Description</label>
                     <div className="control">
-                        <textarea className="textarea" name="description" rows="5">
-                            {editing ? post.description : ""}</textarea>
+                        <textarea className="textarea" name="description" rows="5" onChange={(e) => setDescription(e.target.value)}>
+                            {editing ? post.description : description}</textarea>
                     </div>
                 </div>
 
@@ -40,7 +51,11 @@ function AddPost() {
 
 
                 <div className="is-flex is-justify-content-center mt-4">
-                    <button className="button is-success" type="submit">
+                    <button 
+                    className={`button ${disabled && 'is-danger'} ${!disabled && 'is-success'}`}
+                    type="submit"
+                    disabled={disabled}
+                    >
                         <span className="icon is-small">
                             {editing?(<i className="fas fa-check"></i>):(<i className="fas fa-plus"></i>)}
                         </span>
