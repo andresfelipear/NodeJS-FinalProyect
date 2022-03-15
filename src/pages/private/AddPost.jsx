2cf3110e05f86688ac5eceaa701c86553ba13b39
Andres Arevalo <andresfelipear@gmail.com>
2022-03-15 03:44:37 -0700

    fixed form addPost: select for choose img

--- src/pages/private/AddPost.jsx
+++ src/pages/private/AddPost.jsx
@@ -1,22 +1,22 @@
-import React, { useState, useEffect, useContext,useCallback } from 'react'
-import { UserContext} from "../../context/UserContext";
+import React, { useState, useEffect, useContext, useCallback } from 'react'
+import { UserContext } from "../../context/UserContext";
 import { useLocation, useNavigate, useParams } from 'react-router-dom'
 
 
 function AddPost() {
 
     //query string edit
-    const {search} = useLocation();
+    const { search } = useLocation();
     const myUrl = new URLSearchParams(search)
     const param = myUrl.get('edit')
 
     //param postId
-    const {postId} = useParams();
+    const { postId } = useParams();
 
     const [title, setTitle] = useState("")
     const [imageUrl, setImageUrl] = useState("")
     const [description, setDescription] = useState("")
-    const [editing, setEditing] = useState(param?param:false)
+    const [editing, setEditing] = useState(param ? param : false)
     const [disabled, setDisabled] = useState(true);
     const [status, setStatus] = useState("");
     const [userContext, setUserContext] = useContext(UserContext);
@@ -27,28 +27,28 @@ function AddPost() {
 
     const fetchPost = useCallback(() => {
         fetch(process.env.REACT_APP_API_ENDPOINT + `api/admin/getPost/${postId}`, {
-          method: "GET",
-          credentials: "include",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${userContext.token}`,
-          },
+            method: "GET",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${userContext.token}`,
+            },
         }).then(async (response) => {
-          if (response.ok) {
-            const data = await response.json();
-            setPost(data.post);
-            setTitle(data.post.title);
-            setImageUrl(data.post.imageUrl);
-            setDescription(data.post.description);
-          }
+            if (response.ok) {
+                const data = await response.json();
+                setPost(data.post);
+                setTitle(data.post.title);
+                setImageUrl(data.post.imageUrl);
+                setDescription(data.post.description);
+            }
         });
-      }, [postId, userContext.token])
-    
-      useEffect(() => {
+    }, [postId, userContext.token])
+
+    useEffect(() => {
         if (editing) {
-          fetchPost();
+            fetchPost();
         }
-      }, [editing, fetchPost]);
+    }, [editing, fetchPost]);
 
 
     useEffect(() => {
@@ -59,8 +59,8 @@ function AddPost() {
 
     const submit = () => {
         const body = { title, description, imageUrl, postId };
-        if(!editing){
-            fetch(process.env.REACT_APP_API_ENDPOINT+"api/admin/add-post", {
+        if (!editing) {
+            fetch(process.env.REACT_APP_API_ENDPOINT + "api/admin/add-post", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
@@ -83,8 +83,8 @@ function AddPost() {
                     setStatus("error");
                 });
         }
-        else{
-            fetch(process.env.REACT_APP_API_ENDPOINT+"api/admin/edit-post", {
+        else {
+            fetch(process.env.REACT_APP_API_ENDPOINT + "api/admin/edit-post", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
@@ -107,8 +107,8 @@ function AddPost() {
                     setStatus("error");
                 });
         }
-        
-        
+
+
 
     };
 
@@ -122,16 +122,32 @@ function AddPost() {
                         <input className="input" type="text" name="title" value={title} onChange={(e) => setTitle(e.target.value)} />
                     </div>
                 </div>
-                <div className="field">
-                    <label className="label" htmlFor="title">Image (url)</label>
-                    <div className="control">
-                        <input className="input" type="text" name="imageUrl" value={imageUrl} onChange={(e) => setImageUrl(e.target.value)} />
+                <label className="label" htmlFor="imageUrl">Image</label>
+                <div className="field has-addons">
+
+                    <div className="control has-icons-left">
+                        <div className="select">
+                            <select onChange={(e) => setImageUrl(e.target.value)}>
+                                <option value="img1">Image 1</option>
+                                <option value="img2">Image 2</option>
+                                <option value="img3">Image 3</option>
+                                <option value="img4">Image 4</option>
+                                <option value="img5">Image 5</option>
+                                <option value="img6">Image 6</option>
+                                <option value="img7">Image 7</option>
+                                <option value="img8">Image 8</option>
+                            </select>
+                        </div>
+                        <div className="icon is-large is-left has-text-info">
+                            <i className={`fas fa-image`}></i>
+                        </div>
                     </div>
                 </div>
+
                 <div className="field">
                     <label className="label" htmlFor="title">Description</label>
                     <div className="control">
-                        <textarea type="textarea" className="textarea" name="description" value={description} onChange={(e) => setDescription(e.target.value)}/>
+                        <textarea type="textarea" className="textarea" name="description" value={description} onChange={(e) => setDescription(e.target.value)} />
                     </div>
                 </div>
 
